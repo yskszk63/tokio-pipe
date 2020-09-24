66e04d9413f617b3bc1ef0c42724640a4793cea1
yskszk63 <yskszk63@gmail.com>
2020-09-24 12:48:07 +0900

    bump version

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/tokio-pipe/0.1.4")]
+#![doc(html_root_url = "https://docs.rs/tokio-pipe/0.1.5")]
 //! Asynchronous pipe(2) library using tokio.
 //!
 //! # Example
